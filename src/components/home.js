d84f9103db40897a2f78f5c3d02ffdab05a180f8
davegordon34 <davegordon34@live.com>
2021-01-10 16:14:05 -0700

    getting the search query for the result page

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+
 import Logo from './logo';
 import SearchBar from './searchbar';
 import RecentPosts from './recentPosts';
 
-export default class Home extends Component {
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class Home extends Component {
 
   handleSearchBarSubmit(query) {
-    console.log("trying the handle submit for query", query);
+    this.props.fetchPostsWithQuery(query);
+
     this.props.history.push('/results');
 }
 
@@ -22,3 +27,5 @@ export default class Home extends Component {
     );
   }
 }
+
+export default connect(null, actions)(Home);
--- src/components/results.js
+++ src/components/results.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 import Logo from './logo';
 import SearchBar from './searchbar';
 
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 class Results extends Component {
 
     handleSearchBarSubmit(query) {
@@ -19,4 +22,4 @@ class Results extends Component {
     }
 }
 
-export default Results;
+export default connect(null, actions)(Results);

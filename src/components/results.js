3063aa52770714a05bec550b55d87210dedaf524
davegordon34 <davegordon34@live.com>
2021-01-10 15:34:29 -0700

    updated the code for being able to search in the result page

--- src/components/results.js
+++ src/components/results.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+
 import Logo from './logo';
 import SearchBar from './searchbar';
 
 class Results extends Component {
+
+    handleSearchBarSubmit(query) {
+        console.log(query);
+    }
+
     render() {
         return (
             <div>
                 <Logo size={55}/>
-                <SearchBar/>
+                <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
             </div>
         )
     }

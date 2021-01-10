5a5b2e78654c89f7ba9bdbb2cc7ad8f66bf7cc73
davegordon34 <davegordon34@live.com>
2021-01-10 16:35:39 -0700

    setting up the result information when queried from the search bar

--- src/components/results.js
+++ src/components/results.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Logo from './logo';
 import SearchBar from './searchbar';
+import ResultsPosts from './resultsPosts';
 
 import { connect } from 'react-redux';
 import * as actions from '../actions';
@@ -9,7 +10,7 @@ import * as actions from '../actions';
 class Results extends Component {
 
     handleSearchBarSubmit(query) {
-        console.log(query);
+        this.props.fetchPostsWithQuery(query);
     }
 
     render() {
@@ -17,6 +18,7 @@ class Results extends Component {
             <div>
                 <Logo size={55}/>
                 <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
+                <ResultsPosts/>
             </div>
         )
     }

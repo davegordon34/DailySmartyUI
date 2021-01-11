b428491142c5a9ed6845adc9afa2c5f70f7e36c2
davegordon34 <davegordon34@live.com>
2021-01-10 20:52:46 -0700

    updating the results portion, created a hidden link in the logo to return to the homepage from the results page.

--- src/components/resultsPosts.js
+++ src/components/resultsPosts.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
+import Post from './post';
 
 class ResultsPosts extends Component {
+
+    renderPosts() {
+        const posts = this.props.posts.map((post, index) => {
+            return <Post key={index} {...post}/>
+        })
+        return posts;
+    }
+
     render() {
-        return(
+        return (
             <div className="results-posts">
-               <div className="result-posts-wrapper">
-                   <ul className="results-posts-posts">
-                        results go here
-                   </ul>
-               </div> 
+                <div className="results-posts__wrapper">
+                    <ul className="results-posts__posts">
+                        {this.renderPosts()}
+                    </ul>
+                </div>
             </div>
         )
     }
 }
 
 function mapStateToProps(state) {
-    return { state }
+    return { 
+        posts: state.posts.resultsPosts
+    }
 }
 
 export default connect(mapStateToProps)(ResultsPosts);
--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -3,25 +3,25 @@ import {
     SET_RESULTS_POSTS
 } from '../actions/types';
 
-const init_state = {
-    recentPosts: [],
-    resultsPost: [], 
+const INIT_STATE = {
+    resultsPosts: [],
+    recentPosts: []
 }
 
-export default function(state = init_state, action) {
+export default function(state = INIT_STATE, action) {
     switch (action.type) {
         case SET_RECENT_POSTS:
             const recentPosts = action.payload;
             return {
-                ...state, 
-                recentPosts 
-            }
+                    ...state,
+                    recentPosts
+                }
         case SET_RESULTS_POSTS:
-            const resultsPosts = action.payload;
-            return {
-                ...state,
-                resultsPosts
-            }
+                const resultsPosts = action.payload;
+                return {
+                    ...state, 
+                    resultsPosts
+                }
         default:
             return state;
     }

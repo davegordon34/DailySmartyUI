7fd4fa502047e85ce392c614c6776600038468c0
davegordon34 <davegordon34@live.com>
2021-01-12 21:31:28 -0700

    styled the results-posts page

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -10,7 +10,7 @@ class SearchBar extends Component {
     }
 
     renderInput(field) {
-        return <input type="text" placeholder="&8xf002; Search DailySmarty" {...field.input} />
+        return <input type="text" placeholder="&#xf002; Search DailySmarty" {...field.input} />
         }
 
     render () {
